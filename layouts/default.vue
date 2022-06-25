06933d6c41ae2c38cfb7d7489c3c345cc8bba041
abusiraj84 <abusiraj84@gmail.com>
2022-06-25 13:52:24 +0300

    push files to Git

---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <client-only>
+      <div
+        class="justify container mx-auto flex flex-col items-center gap-[40px] pt-[100px]"
+      >
+        <div
+          class="mt-[50px] w-fit cursor-pointer rounded-[8px] bg-[#000] py-[6px] px-[11px] text-inverted hover:brightness-125"
+          @click="changeLanguage"
+        >
+          {{ $t('language') }}
+        </div>
+        <div :dir="lang === 'en' ? 'ltr' : 'rtl'">
+          <Nuxt />
+        </div>
+      </div>
+    </client-only>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      lang: 'ar',
+    }
+  },
+  head: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {
+    changeLanguage() {
+      if (this.lang === 'ar') {
+        this.lang = 'en'
+        this.$i18n.locale = 'en'
+      } else {
+        this.lang = 'ar'
+        this.$i18n.locale = 'ar'
+      }
+    },
+  },
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="flex bg-base">
+      <div class="min-h-[300px] w-[300px] rounded-[15px] bg-muted p-[20px]">
+        <!-- Start Heare -->
+        {{ $t('Start-from-here') }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {}
+  },
+  head: {},
+  computed: {},
+  watch: {},
+  mounted() {},
+  methods: {},
+}
+</script>
